e5bc47c20fbb4e472014669b05372c842f072031
Admin <758378737@qq.com>
2020-06-24 17:30:36 +0800

    修改手机号+密码注册

--- app/src/main/java/com/guider/healthring/activity/RegisterActivity2.java
+++ app/src/main/java/com/guider/healthring/activity/RegisterActivity2.java
@@ -59,8 +59,8 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Func1;
 
 /**
- * Created by thinkpad on 2017/3/4.
- * 注册页面
+ *
+ * 手机号注册页面
  */
 
 public class RegisterActivity2 extends WatchBaseActivity implements RequestView {
@@ -218,38 +218,56 @@ public class RegisterActivity2 extends WatchBaseActivity implements RequestView 
             case R.id.login_btn_reger:  //注册
                 //中文状态为手机号
                 String phoneStr = username.getText().toString().trim(); //手机号
-                String phoneCode = tv_phone_head.getText().toString();  //验证码
+                //String phoneCode = tv_phone_head.getText().toString();  //验证码
                 String phonePwd = password.getText().toString();        //密码
-                if(WatchUtils.isEmpty(phoneStr) || WatchUtils.isEmpty(phoneCode) || WatchUtils.isEmpty(phonePwd))
+//                if(WatchUtils.isEmpty(phoneStr) || WatchUtils.isEmpty(phoneCode) || WatchUtils.isEmpty(phonePwd))
+//                    return;
+//                //registerRemote(phoneStr,phoneCode);
+//                registerRemote(phoneStr,phoneCode,phonePwd);
+
+                if(WatchUtils.isEmpty(phoneStr)){
+                    ToastUtil.showToast(this,getResources().getString(R.string.input_email));
                     return;
-                //registerRemote(phoneStr,phoneCode);
-                registerRemote(phoneStr,phoneCode,phonePwd);
+                }
+                if(!StringUtils.isNumeric(phoneStr)){
+                    ToastUtil.showToast(this,getResources().getString(R.string.format_is_wrong));
+                    return;
+                }
 
-
-
-
-
-
-
-
-
-//                if (!WatchUtils.isEmpty(phoneTxt) && !WatchUtils.isEmpty(pwdText)) {
-//                    if (pwdText.length() > 5) {
-//                        if (!WatchUtils.isEmpty(code) && VerifyUtil.checkNumber(code)) {  //验证码
-//                            // 提交验证码，其中的code表示验证码，如“1357”
-//                            SMSSDK.submitVerificationCode(StringUtils.substringAfter(phoneCode,"+").trim(), phoneStr, code);
-//                        } else {
-//                            ToastUtil.showShort(RegisterActivity2.this, getResources().getString(R.string.string_code_null));
-//                        }
-//                    } else {
-//                        ToastUtil.showShort(RegisterActivity2.this, getResources().getString(R.string.not_b_less));
-//                    }
-//                } else {
-//                    ToastUtil.showShort(RegisterActivity2.this, getResources().getString(R.string.string_phone_pass_null));
-//                }
-
+                if(WatchUtils.isEmpty(phonePwd)){
+                    ToastUtil.showToast(this,getResources().getString(R.string.input_password));
+                    return;
+                }
+                //手机号注册，手机号+密码
+                registerForNoCode(phoneStr,phonePwd);
                 break;
         }
+    }
+
+    /**
+     * 手机号+密码注册，不需要验证码
+     * @param phoneStr 手机号
+     * @param phonePwd 密码
+     */
+    private void registerForNoCode(String phoneStr, String phonePwd) {
+        try {
+            Gson gson = new Gson();
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("phone", phoneStr);
+            map.put("pwd", Md5Util.Md532(phonePwd));
+            map.put("status", "0");
+            map.put("type", "0");
+            String mapjson = gson.toJson(map);
+            Log.e("msg", "-mapjson-" + mapjson);
+            if (requestPressent != null) {
+                requestPressent.getRequestJSONObject(0x02, Commont.FRIEND_BASE_URL + URLs.myHTTPs, RegisterActivity2.this, mapjson, 2);
+                //注册盖德账号
+                registerGuiderAccount();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
 
@@ -301,30 +319,6 @@ public class RegisterActivity2 extends WatchBaseActivity implements RequestView 
             requestPressent.getRequestJSONObject(0x01,urls,RegisterActivity2.this,mapjson,1);
 
         }
-
-
-
-
-
-
-//        if (!WatchUtils.isEmpty(pwdText) && !WatchUtils.isEmpty(phoneTxt)) {
-//            Gson gson = new Gson();
-//            HashMap<String, Object> map = new HashMap<>();
-//            map.put("phone", phoneTxt);
-//            map.put("pwd", Md5Util.Md532(pwdText));
-//            map.put("status", "0");
-//            map.put("type", "0");
-//            String mapjson = gson.toJson(map);
-////        Log.e("msg", "-mapjson-" + mapjson);
-////            dialogSubscriber = new DialogSubscriber(subscriberOnNextListener, RegisterActivity2.this);
-////            OkHttpObservable.getInstance().getData(dialogSubscriber, URLs.HTTPs + URLs.myHTTPs, mapjson);
-//            if(requestPressent != null){
-//                requestPressent.getRequestJSONObject(0x01,URLs.HTTPs + URLs.myHTTPs,RegisterActivity2.this,mapjson,1);
-//
-//            }
-//
-//        }
-
     }
 
 
@@ -338,98 +332,6 @@ public class RegisterActivity2 extends WatchBaseActivity implements RequestView 
             return;
         }
 
-//        EventHandler eventHandler = new EventHandler() {
-//            public void afterEvent(int event, int result, Object data) {
-//                // afterEvent会在子线程被调用，因此如果后续有UI相关操作，需要将数据发送到UI线程
-//                Message msg = new Message();
-//                msg.arg1 = event;
-//                msg.arg2 = result;
-//                msg.obj = data;
-//                new Handler(Looper.getMainLooper(), new Handler.Callback() {
-//                    @Override
-//                    public boolean handleMessage(Message msg) {
-//                        int event = msg.arg1;
-//                        int result = msg.arg2;
-//                        Object data = msg.obj;
-//
-//                        Log.e(TAG,"-------event="+event+"---result="+result+"---data="+data.toString());
-//
-//
-//                        if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE) {
-//                            if (result == SMSSDK.RESULT_COMPLETE) {
-//                                // TODO 处理成功得到验证码的结果
-//                                // 请注意，此时只是完成了发送验证码的请求，验证码短信还需要几秒钟之后才送达
-////                                                Log.e("===验证码", "完成了发送验证码的请求，验证码短信还需要几秒钟之后才送达  " + data.toString());
-//
-//                            } else {
-//                                if (!WatchUtils.isEmpty(data.toString()) && data.toString().contains("java.lang.Throwable:")) {
-//                                    String res = data.toString().replace("java.lang.Throwable:", "").trim();
-////                                                    Log.e("===验证码", "处理错误的结果  " + res);
-//                                    if (!WatchUtils.isEmpty(res)) {
-//                                        CodeBean codeBean = new Gson().fromJson(res, CodeBean.class);
-//                                        if (codeBean != null) {
-//                                            int status = codeBean.getStatus();
-//                                            if (status == 603) {//手机号错
-//                                                ToastUtil.showLong(RegisterActivity2.this, getResources().getString(R.string.string_phone_er));
-//                                            } else if (status == 468) {//验证码错
-//                                                ToastUtil.showLong(RegisterActivity2.this, getResources().getString(R.string.yonghuzdffhej));
-//                                            } else if (status == 457) {//手机号格式不对
-//                                                username.setText("");
-//                                                sendBtn.setText(getResources().getString(R.string.resend));
-//                                                sendBtn.setClickable(true);
-//                                                ToastUtil.showShort(RegisterActivity2.this, getResources().getString(R.string.format_is_wrong));
-//                                            } else {
-//                                                ToastUtil.showLong(RegisterActivity2.this, codeBean.getError());
-//                                            }
-//                                        }
-//                                    }
-//                                }
-//                                // TODO 处理错误的结果
-//                                ((Throwable) data).printStackTrace();
-//                            }
-//                        } else if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
-//                            if (result == SMSSDK.RESULT_COMPLETE) {
-//                                // TODO 处理验证码验证通过的结果
-//                                Log.e("===验证码", "处理验证码验证通过的结果  " + data.toString());
-//                                registerRemote();
-//                                //注册盖德的账号
-//                                registerGuiderAccount();
-//
-//                            } else {
-//                                if (!WatchUtils.isEmpty(data.toString()) && data.toString().contains("java.lang.Throwable:")) {
-//                                    String res = data.toString().replace("java.lang.Throwable:", "").trim();
-////                                                    Log.e("===验证码", "处理错误的结果2  " + res);
-//                                    if (!WatchUtils.isEmpty(res)) {
-//                                        CodeBean codeBean = new Gson().fromJson(res, CodeBean.class);
-//                                        if (codeBean != null) {
-//                                            int status = codeBean.getStatus();
-////                                                            Log.e("===验证码", "处理错误的结果2  " + status);
-//                                            if (status == 468) {//验证码错
-//                                                ToastUtil.showLong(RegisterActivity2.this, getResources().getString(R.string.yonghuzdffhej));
-//                                            } else if (status == 457) {//手机号格式不对
-//                                                username.setText("");
-//                                                sendBtn.setText(getResources().getString(R.string.resend));
-//                                                sendBtn.setClickable(true);
-//                                                ToastUtil.showShort(RegisterActivity2.this, getResources().getString(R.string.format_is_wrong));
-//                                            } else {
-//                                                ToastUtil.showLong(RegisterActivity2.this, codeBean.getError());
-//                                            }
-//                                        }
-//                                    }
-//                                }
-//                                // TODO 处理错误的结果
-//                                ((Throwable) data).printStackTrace();
-//                            }
-//                        }
-////                                        Log.e("===验证码", "其他接口的返回结果也类似  " + event);
-//                        // TODO 其他接口的返回结果也类似，根据event判断当前数据属于哪个接口
-//                        return false;
-//                    }
-//                }).sendMessage(msg);
-//            }
-//        };
-
-
         //输入的手机号码
         String phoneStr = username.getText().toString();
         //选择的区号
@@ -439,29 +341,6 @@ public class RegisterActivity2 extends WatchBaseActivity implements RequestView 
         initTime();
         snedPhoneNumToServer(phoneStr,areaCodeStr);
 
-
-
-
-
-
-
-//        if (WatchUtils.isEmpty(phoneStr)) {
-//            ToastUtil.showShort(RegisterActivity2.this, getResources().getString(R.string.format_is_wrong));
-//        } else {
-//            initTime();
-//
-//
-//            String getPhoneCodeUrl = "http://api.guiderhealth.com/api/v1/phonecode?phone=";
-//            requestPressent.getRequestJSONObjectGet(1002,getPhoneCodeUrl,RegisterActivity2.this,0);
-//
-//
-//            //registerGuiderAccount();
-//
-////            // 注册一个事件回调，用于处理SMSSDK接口请求的结果
-////            SMSSDK.registerEventHandler(eventHandler);
-////            // 请求验证码，其中country表示国家代码，如“86”；phone表示手机号码，如“13800138000”
-////            SMSSDK.getVerificationCode(StringUtils.substringAfter(areaCodeStr,"+").trim(), phoneStr);
-//        }
     }
 
 
@@ -485,11 +364,6 @@ public class RegisterActivity2 extends WatchBaseActivity implements RequestView 
 
 
 
-
-
-
-
-
     //注册盖德的账号
     private void registerGuiderAccount() {
         String phoneStr = username.getText().toString();
@@ -509,7 +383,7 @@ public class RegisterActivity2 extends WatchBaseActivity implements RequestView 
 
     @Override
     public void successData(int what, Object object, int daystag) {
-        Log.e(TAG,"----------what="+what+"--=obj="+object.toString());
+      //  Log.e(TAG,"----------what="+what+"--=obj="+object.toString());
         if(object == null)
             return;
         if (object.toString().contains("<html>"))
@@ -542,40 +416,6 @@ public class RegisterActivity2 extends WatchBaseActivity implements RequestView 
             e.printStackTrace();
         }
 
-
-
-
-
-//        switch (what){
-//            case 0x01:
-//                analysisRegiInfo(object.toString());
-//                break;
-//            case 1001:  //登录盖德后台返回
-//                if(WatchUtils.isNetRequestSuccess(object.toString(),0)){
-//                    try {
-//                        JSONObject jsonObject = new JSONObject(object.toString());
-//                        if(jsonObject.has("data")){
-//                            JSONObject dataJsonObject = jsonObject.getJSONObject("data");
-//                            long accountId = dataJsonObject.getLong("accountId");
-//                            SharedPreferencesUtils.setParam(MyApp.getInstance(), "accountIdGD", accountId);
-//                        }
-//
-//                    }catch (Exception e){
-//                        e.printStackTrace();
-//                    }
-//                }
-//                break;
-//            case 1002:  //获取手机验证码
-//                try {
-//                    JSONObject jsonObject = new JSONObject(object.toString());
-//                    ToastUtil.showToast(RegisterActivity2.this, jsonObject.getString("data")+jsonObject.getString("msg"));
-//                }catch (Exception e){
-//                    e.printStackTrace();
-//                }
-//
-//                break;
-//        }
-
     }
 
     @Override
@@ -587,8 +427,6 @@ public class RegisterActivity2 extends WatchBaseActivity implements RequestView 
     public void closeLoadDialog(int what) {
 
     }
-
-
 
 
     //提交注册返回
@@ -612,12 +450,6 @@ public class RegisterActivity2 extends WatchBaseActivity implements RequestView 
             e.printStackTrace();
         }
     }
-
-
-
-
-
-
 
 
     @Override
